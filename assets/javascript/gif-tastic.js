02b6cd55f4d871d84139180fe2e8b84ce7102be5
Alex Rocha <rocha8524@gmail.com>
2019-10-04 10:56:13 -0400

    Gifs from array appear, working on gifs appearing from search and animation

--- assets/javascript/gif-tastic.js
+++ assets/javascript/gif-tastic.js
@@ -18,7 +18,7 @@ function buttonGenerator() {
 }
 
 // This function handles events where one button is clicked
-$("#add-gif").on("click", function (event) {
+$("#add-button").on("click", function (event) {
 
   // We're using a form so that the user can hit enter instead of clicking the button if they want
   event.preventDefault();
@@ -34,9 +34,8 @@ $("#add-gif").on("click", function (event) {
 // Calling the buttonGenerator function at least once to display the initial list of movies
 buttonGenerator();
 
-// Creating an AJAX call for the specific gif button being clicked
-$("gif-button").on("click", function () {
-  var sports = $(this).attr("data-sports");
+$("button").on("click", function() {
+  var sports = $(this).attr("data-name");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     sports + "&api_key=VOB42k43YwwyvA4LU1bG86RXHJU1eZMQ&limit=10";
 
@@ -45,13 +44,14 @@ $("gif-button").on("click", function () {
     method: "GET"
   })
     .then(function (response) {
+      console.log(response);
+
       var results = response.data;
 
       for (var i = 0; i < results.length; i++) {
         var gifDiv = $("<div>");
 
         var rating = results[i].rating;
-
         var p = $("<p>").text("Rating: " + rating);
 
         var gifImage = $("<img>");
@@ -61,18 +61,18 @@ $("gif-button").on("click", function () {
         gifDiv.prepend(gifImage);
 
         $("#display-gifs").prepend(gifDiv);
-
-        // Pause and animate gifs when clicked
-        $(".gif").on("click", function () {
-          var state = $(this).attr("data-state");
-          if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-          } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-          }
-        });
       }
-    });
+    })
+});
+
+// Pause and animate gifs when clicked
+$("#display-gifs").on("click", function () {
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
 });
